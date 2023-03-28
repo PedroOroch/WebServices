1ba3c55df9913be1d59340297a4a1c015110ef58
PEDRO DE OLIVEIRA ROCHA <pedro.orocha@senacsp.edu.br>
2023-03-27 21:39:46 -0300

    Aprendendo a desenvolver api

--- Senac-DemoMVC/routes/api.php
+++ Senac-DemoMVC/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Http\Controllers\api\simplescontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('simples/receber', [SimplesController::class, 'receber']);
+//'App\Http\Controllers\Api\SimplesController@receber'
